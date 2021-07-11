c19c60c3b2d0efba6e4ac4492f3db878d725c03c
Gideon Crawley <gscrawley@gmail.com>
2021-07-10 21:01:42 -0700

    added three main components

--- App.js
+++ App.js
@@ -14,7 +14,17 @@ export default class App extends Component {
     }
 }
 _onGLContextCreate = async (gl) => {
-  // Here is where we will define our scene, camera and renderer
+// 1. Scene
+var scene = new THREE.Scene(); 
+
+// 2. Camera
+const camera = new THREE.PerspectiveCamera(
+  75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);
+
+// 3. Renderer
+const renderer = ExpoTHREE.createRenderer({ gl });
+renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
 }
 
 
